6adeaa2ab0591acf380a08e499db85d895211434
Saurav <sp.gharti@gmail.com>
2019-08-13 15:13:08 +0545

    update model fields with script

--- second/models.py
+++ second/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class SecondHello(models.Model):
     name = models.CharField(max_length=100)
+    sex = models.CharField(max_length=20)
     age = models.PositiveSmallIntegerField()
 
 
 class SecondHi(models.Model):
     hello = models.ForeignKey(SecondHello, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True)
