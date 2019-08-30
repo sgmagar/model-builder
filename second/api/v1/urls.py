ca75cc706d96ea99198fe4e0362f7522e045d3f6
Saurav <sp.gharti@gmail.com>
2019-08-31 00:13:43 +0545

    add api urls via script

--- second/api/v1/urls.py
+++ second/api/v1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import SecondHelloViewSet
 
 router = DefaultRouter()
+router.register("secondhello", SecondHelloViewSet)
 
 urlpatterns = [path("", include(router.urls))]
