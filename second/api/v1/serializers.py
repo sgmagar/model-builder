0bf6a3a389b4c1144d6584d33fc45dca6dd026ad
Saurav <sp.gharti@gmail.com>
2019-08-30 22:15:47 +0545

    viewset and serializer generated using script

--- second/api/v1/serializers.py
+++ second/api/v1/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from second.models import SecondHello
+
+
+class SecondHelloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SecondHello
+        fields = "__all__"
--- second/api/v1/viewsets.py
+++ second/api/v1/viewsets.py
@@ -1,1 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from second.models import SecondHello
+from .serializers import SecondHelloSerializer
+
+
+class SecondHelloViewSet(ModelViewSet):
+    serializer_class = SecondHelloSerializer
+    queryset = SecondHello.objects.all()
